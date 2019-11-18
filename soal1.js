bf000e1e920be1fdb62758dfcd76db0ff72a0d17
dhentyarsa <dhentyarsa@gmail.com>
2019-11-18 11:57:37 +0700

    ujian

---
+++ soal1.js
@@ -0,0 +1,22 @@
+var price = 10000
+
+function returnPrice(x){
+        for(var i = 1; i <= x; i++){
+            if(i%4 == 0){
+                var plusone = price*0.1
+                Math.ceil(price += plusone)
+            }else if(i%1 == 0){
+                var plusfour = price*0.2
+                Math.ceil(price += plusfour)
+            }
+        }
+        if(price < 30000000){
+            console.log(`menit ke ${i-1} harga ${Math.ceil(price)}`)
+        }else if(price > 30000000){
+            console.log(`menit ke ${i-1} barang sudah terjual`)
+        }
+        
+    }
+
+
+console.log(returnPrice(49))a
---
+++ soal3.js
@@ -0,0 +1,8 @@
+var coin = [25,10,5,1]
+
+function coinCheck(num){
+    for(var i = 0; i<coin.length; i++){
+        
+    }
+
+}
